46ea046a703a2562662be9193049cd6930278675
ada024 <andreasm.dale@bkkfiber.no>
2022-02-28 15:54:41 +0100

    Added read from db

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import MEETUPS_DATA from "../meetup.data";
 import MeetupList from "../components/meetups/MeetupList";
+import {MongoClient} from "mongodb";
+import {dbConfig} from "../dbconfig";
 
 
 function HomePage(props) {
@@ -8,11 +10,30 @@ function HomePage(props) {
 
 // Special function, rands before regular function (pre-renders)
 export async function getStaticProps() {
+    try {
+        const client = await MongoClient.connect(dbConfig);
+        const db = client.db();
+
+        const meetupsCollection = db.collection('meetups');
+
+        const meetups = await meetupsCollection.find().toArray();
+        await client.close();
+    } catch (e) {
+        console.log(e);
+    }
+
     return {
         props: {
-            meetups: MEETUPS_DATA
-        }
+            meetups: meetups.map((meetup) => ({
+                title: meetup.data.title,
+                address: meetup.data.address,
+                image: meetup.data.image,
+                id: meetup._id.toString(),
+            })),
+            revalidate: 1,
+        },
     };
+
 }
 
 export default HomePage;
